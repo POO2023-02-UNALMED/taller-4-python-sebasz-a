3cc1beb72f1872394fa07c21b7c5ff3700ac3a6e
sebasz-a <jpabuena@unal.edu.co>
2023-08-31 16:45:08 -0500

    Prueba 1

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,11 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon=None):
         self._nombre = nombre
+        if salon == None:
+            self._salon = "remoto"
+            return
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return self._nombre + " " + self._salon
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,32 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        if asignaturas == None:
+            self._asignaturas = []
+        if estudiantes == None:
+            self.listadoAlumnos = []
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista == None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return "Grupo de estudiantes: " + self._grupo 
+
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre=None):
+        if nombre == None:
+            cls.grado = "Grado 6"
+            return
         cls.grado = nombre
